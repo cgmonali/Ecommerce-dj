3d972876e27ff59128b0e9ac94611775e56d174d
monali <monali.gowda@galepartners.com>
2024-12-26 09:16:23 +0530

    implement checkout functionality and integrate it to the frontend

--- Cart/urls.py
+++ Cart/urls.py
@@ -5,5 +5,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('cart/add', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.cart_view, name='cart_view'),   
+    path('cart/', views.cart_view, name='cart_view'),
+    path('cart/checkout', views.checkout, name='checkout'),  
+    path('cart/clear_store', views.clear_store, name='clear_store'),
 ]
